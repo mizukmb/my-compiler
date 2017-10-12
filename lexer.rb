786a1c2f906932bb1d69d0e43b7358628c6edc6a
mizukmb <mizukmb6@gmail.com>
2017-10-12 21:26:47 +0900

    Allow space, line

--- lexer.rb
+++ lexer.rb
@@ -1,26 +1,61 @@
 class MyLexer
   class MyLexerError < StandardError; end
 
-  attr_reader :str
-
   def initialize(str)
     @str = str
+    @index = 0
+    @ret = []
   end
 
   def run
-    if str.match number_pattern
-      ["lit", str]
-    else
-      raise MyLexerError
+    @str.each_char do |char|
+      break if char == "\n"
+
+      if char =~ space_or_line
+        next_word!
+        next
+      end
+
+      # 数値リテラルチェック
+      break unless @ret.empty?
+      @str[@index..-1].each_char do |c|
+        if c =~ number_pattern
+          @ret.push c
+          next_word!
+        elsif c =~ space_or_line
+          break
+        else
+          raise MyLexerError
+        end
+      end
     end
+
+    ["lit", @ret.join('')]
   end
 
   private
+  def next_word!
+    @index += 1
+  end
+
   def number_pattern
-    %r(\A\d+\Z)
+    %r(\d)
+  end
+
+  # 半角・全角スペース、タブ、改行
+  def space_or_line
+    %r( |　|\t|\n)
   end
 end
 
-str = "100"
+str = %w(
+0
+100
+  100
+50   
+hoge
+)
 
-p MyLexer.new(str).run
+str.each do |s|
+  p MyLexer.new(s).run
+end
